31acbe611e78ddc27a61baefb012238eb44e12f5
RahemanAli321 <76989478+RahemanAli321@users.noreply.github.com>
2021-03-28 07:25:59 -0700

    Update pattern1.java

--- pattern1.java
+++ pattern1.java
@@ -1,10 +1,12 @@
+import java.util.*;
 public class Pattern {
 
     public static void main(String[] args) {
-        int rows = 5;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        for(int i = 1; i <= rows; ++i) {
-            for(int j = 1; j <= i; ++j) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
